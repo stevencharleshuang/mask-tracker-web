a8f762dc7204bbd7fa1d2bdcae6c432c67e0d960
Steven Huang <steven.charles.huang@gmail.com>
2020-11-03 17:13:37 -0500

    added register new user functionality

--- src/auth/Register.js
+++ src/auth/Register.js
@@ -1,68 +1,114 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { db, auth } from '../constants/firebase';
 
-const Register = (props) => {
-  const [email, setEmail] = useState('');
-  const [fullname, setFullname] = useState('');
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+export default class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: '',
+      isRegistered: false,
+      formData: {
+        email: '',
+        fullname: '',
+        username: '',
+        password: '',
+      },
+    };
+  }
 
-  const handleSubmit = (formData) => {};
+  handleInputChange = (event, target) => {
+    this.setState((prevState) => {
+      prevState.formData[target] = event.target.value;
+    });
+  };
 
-  return (
-    <div>
-      Register
-      <div className="register-form">
-        <input
-          autoCapitalize="off"
-          autoCorrect="off"
-          maxLength="75"
-          name="email"
-          placeholder="email"
-          type="text"
-          className="register register-email"
-          onChange={() => {}}
-          value={email}
-        />
-        <input
-          autoCapitalize="off"
-          autoCorrect="off"
-          maxLength="75"
-          name="fullname"
-          placeholder="fullname"
-          type="text"
-          className="register register-fullname"
-          onChange={() => {}}
-          value=""
-        />
-        <input
-          autoCapitalize="off"
-          autoCorrect="off"
-          maxLength="75"
-          name="username"
-          placeholder="username"
-          type="text"
-          className="register register-username"
-          onChange={() => {}}
-          value=""
-        />
-        <input
-          autoCapitalize="off"
-          autoCorrect="off"
-          maxLength="75"
-          name="email"
-          placeholder="password"
-          type="password"
-          className="register register-password"
-          onChange={() => {}}
-          value=""
-        />
-      </div>
+  handleSubmit = async () => {
+    try {
+      const newUser = await auth.createUserWithEmailAndPassword(
+        this.state.formData.email,
+        this.state.formData.password
+      );
+
+      if (newUser.user.uid) {
+        const uid = newUser.user.uid;
+        const { email, fullname, username } = this.state.formData;
+        const user = {
+          email,
+          fullname,
+          uid,
+          username,
+        };
+
+        await db.doc(`/users/${uid}`).set(user, { merge: true });
+      }
+    } catch (error) {
+      console.error(error.message);
+      this.setState({ error: error.message });
+    }
+  };
+
+  render = () => {
+    return (
       <div>
-        Have an account? <Link to="/">Log In</Link>
+        Register
+        <div className="register-form">
+          <input
+            autoCapitalize="off"
+            autoCorrect="off"
+            maxLength="75"
+            name="email"
+            placeholder="email"
+            type="text"
+            className="register register-email"
+            onChange={(e) => this.handleInputChange(e, e.currentTarget.name)}
+            value={this.state.email}
+          />
+          <input
+            autoCapitalize="off"
+            autoCorrect="off"
+            maxLength="75"
+            name="fullname"
+            placeholder="fullname"
+            type="text"
+            className="register register-fullname"
+            onChange={(e) => this.handleInputChange(e, e.currentTarget.name)}
+            value={this.state.fullname}
+          />
+          <input
+            autoCapitalize="off"
+            autoCorrect="off"
+            maxLength="75"
+            name="username"
+            placeholder="username"
+            type="text"
+            className="register register-username"
+            onChange={(e) => this.handleInputChange(e, e.currentTarget.name)}
+            value={this.state.username}
+          />
+          <input
+            autoCapitalize="off"
+            autoCorrect="off"
+            maxLength="75"
+            name="password"
+            placeholder="password"
+            type="password"
+            className="register register-password"
+            onChange={(e) => this.handleInputChange(e, e.currentTarget.name)}
+            value={this.state.password}
+          />
+        </div>
+        <div className="button-wrapper">
+          <button onClick={this.handleSubmit}>Sign Up</button>
+        </div>
+        <div className="error-container">
+          {this.state.error && <h4>{this.state.error}</h4>}
+        </div>
+        <div>
+          Have an account? <Link to="/">Log In</Link>
+        </div>
+        {this.isRegistered && <Redirect to="/login" />}
       </div>
-    </div>
-  );
-};
-
-export default Register;
+    );
+  };
+}
--- src/constants/firebase.js
+++ src/constants/firebase.js
@@ -5,14 +5,14 @@ require('firebase/storage');
 require('dotenv').config();
 
 const devConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // DEV TESTING (Comment out for staging / production)
@@ -21,4 +21,5 @@ firebase.initializeApp(devConfig);
 const db = firebase.firestore();
 const auth = firebase.auth();
 const storage = firebase.storage();
-export { db, auth, storage };
+
+export { db, auth, storage, firebase };
